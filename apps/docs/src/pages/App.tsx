f2cb650b6c4a4410d56fd9077bbcd2c4eb72ee75
Sylas Xu <xymzhs@gmail.com>
2024-06-18 23:33:05 +0800

    feat: remove servers

---
+++ apps/docs/src/pages/App.tsx
@@ -0,0 +1,6 @@
+
+const App= ()=>{
+    return <div>1</div>
+}
+
+export default App
--- servers/src/common/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import { Response } from "express";
-
-export function error(res:Response,code:number,message:string){
-   return res.status(code).json({ message: message }) 
-}
